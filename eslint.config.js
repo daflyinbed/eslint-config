fb3d44a42815e2b63f4b5bdcdee08b64996f90f6
daflyinbed <1677759063@qq.com>
2024-08-11 18:32:44 +0800

    chore: fmt

--- eslint.config.js
+++ eslint.config.js
@@ -2,4 +2,4 @@
 // eslint-disable-next-line antfu/no-import-dist
 import { xwbx } from "./dist/index.js";
 
-export default xwbx({ typescript: true, vue: true, astro: true, unocss: true});
+export default xwbx({ typescript: true, vue: true, astro: true, unocss: true });
