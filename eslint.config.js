1e9afcb45eb580e002e365cd21be946ca048e766
夕舞八弦 <1677759063@qq.com>
2024-08-16 09:43:46 +0800

    chore: update config

--- eslint.config.js
+++ eslint.config.js
@@ -2,4 +2,10 @@
 // eslint-disable-next-line antfu/no-import-dist
 import { xwbx } from "./dist/index.js";
 
-export default xwbx({ typescript: true, vue: true, astro: true, unocss: true });
+export default xwbx({
+  ignores: ["fixtures"],
+  typescript: true,
+  vue: true,
+  astro: true,
+  unocss: true,
+});
