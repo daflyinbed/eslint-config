7f850e9f1a030cb251cda620a5bbac7727b3e4df
夕舞八弦 <1677759063@qq.com>
2024-08-28 19:08:33 +0800

    fix: `isInEditorEnv` should be false when in git hooks

--- src/utils.ts
+++ src/utils.ts
@@ -145,12 +145,23 @@ export async function ensurePackages(
 }
 
 export function isInEditorEnv(): boolean {
+  if (process.env.CI) return false;
+  if (isInGitHooksOrLintStaged()) return false;
   return !!(
-    (process.env.VSCODE_PID ||
-      process.env.VSCODE_CWD ||
-      process.env.JETBRAINS_IDE ||
-      process.env.VIM ||
-      process.env.NVIM) &&
-    !process.env.CI
+    false ||
+    process.env.VSCODE_PID ||
+    process.env.VSCODE_CWD ||
+    process.env.JETBRAINS_IDE ||
+    process.env.VIM ||
+    process.env.NVIM
+  );
+}
+
+export function isInGitHooksOrLintStaged(): boolean {
+  return !!(
+    false ||
+    process.env.GIT_PARAMS ||
+    process.env.VSCODE_GIT_COMMAND ||
+    process.env.npm_lifecycle_script?.startsWith("lint-staged")
   );
 }
