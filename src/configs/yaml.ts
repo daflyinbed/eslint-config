80b07fafad1c6b8a2cb760f2aa6bb88b9412c8cf
夕舞八弦 <1677759063@qq.com>
2025-04-07 17:44:02 +0800

    feat: sort all pnpm-workspace keys

--- src/configs/yaml.ts
+++ src/configs/yaml.ts
@@ -69,6 +69,10 @@ export async function yaml(
             ],
             pathPattern: "^$",
           },
+          {
+            order: { type: "asc" },
+            pathPattern: ".*",
+          },
         ],
       },
     },
