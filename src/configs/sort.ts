caa9c8022c5d343ac8a9ec801de5b4265ecea08e
夕舞八弦 <1677759063@qq.com>
2025-07-28 10:37:53 +0800

    feat: support sort jsconfig

--- src/configs/sort.ts
+++ src/configs/sort.ts
@@ -114,7 +114,7 @@ export async function sortPackageJson(): Promise<TypedFlatConfigItem[]> {
 export function sortTsconfig(): TypedFlatConfigItem[] {
   return [
     {
-      files: ["**/tsconfig.json", "**/tsconfig.*.json"],
+      files: ["**/[jt]sconfig.json", "**/[jt]sconfig.*.json"],
       name: "xwbx/sort/tsconfig-json",
       rules: {
         "jsonc/sort-keys": [
