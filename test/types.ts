97fe95aff64f2cba0c9b4a90225fa96ac9c65a24
夕舞八弦 <1677759063@qq.com>
2025-09-05 11:28:21 +0800

    chore: fix types

--- test/types.ts
+++ test/types.ts
@@ -3,5 +3,5 @@ import type { Linter } from "eslint";
 
 // Make sure they are compatible
 ((): Linter.Config => ({}) as TypedFlatConfigItem)();
-// @ts-expect-error not working well for some reason
+
 ((): TypedFlatConfigItem => ({}) as Linter.Config)();
