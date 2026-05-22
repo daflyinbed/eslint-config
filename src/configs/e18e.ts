bcc2290356f50720fe678b41df08c1dd480a1725
夕舞八弦 <1677759063@qq.com>
2026-05-22 17:04:34 +0800

    fix: disable some e18e rules

--- src/configs/e18e.ts
+++ src/configs/e18e.ts
@@ -44,6 +44,13 @@ export async function e18e(
         ...(performanceImprovements
           ? { ...configs.performanceImprovements?.rules }
           : {}),
+
+        // these are a bit opinionated and dangerous, so we'll disable them for now
+        "e18e/prefer-array-to-reversed": "off",
+        "e18e/prefer-array-to-sorted": "off",
+        "e18e/prefer-array-to-spliced": "off",
+        "e18e/prefer-spread-syntax": "off",
+
         ...overrides,
       },
     },
