f1139a2ec854959a62dedbf4702f4d5eab1cbaa3
夕舞八弦 <1677759063@qq.com>
2026-05-22 16:24:09 +0800

    fix: e18e/ban-dependencies defaults to warn level

--- src/configs/e18e.ts
+++ src/configs/e18e.ts
@@ -28,7 +28,17 @@ export async function e18e(
       },
       rules: {
         ...(modernization ? { ...configs.modernization.rules } : {}),
-        ...(moduleReplacements ? { ...configs.moduleReplacements?.rules } : {}),
+        ...(moduleReplacements
+          ? Object.fromEntries(
+              // moduleReplacements only has e18e/ban-dependencies one rule
+              Object.entries(configs.moduleReplacements?.rules ?? {}).map(
+                ([key, value]) => [
+                  key,
+                  value === "error" || value === 2 ? "warn" : value,
+                ],
+              ),
+            )
+          : {}),
         ...(performanceImprovements
           ? { ...configs.performanceImprovements?.rules }
           : {}),
