84711cbac3051f197afbf37af5c6d4b6d6ddf715
夕舞八弦 <1677759063@qq.com>
2026-05-25 15:20:50 +0800

    chore: add tsnapi

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from "tsdown";
+import ApiSnapshot from "tsnapi/rolldown";
 
 export default defineConfig({
   entry: ["src/index.ts"],
+  dts: true,
   shims: true,
   format: ["esm"],
   exports: true,
+  plugins: [
+    ApiSnapshot({
+      outputDir: "test/__snapshots__/api",
+    }),
+  ],
 });
