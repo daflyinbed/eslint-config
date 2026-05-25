588c1f9a8c16d19a70b79e4de8ddedcb95fad345
夕舞八弦 <1677759063@qq.com>
2026-05-25 15:42:28 +0800

    feat(svelte): use recommended rules

--- src/configs/svelte.ts
+++ src/configs/svelte.ts
@@ -4,6 +4,7 @@ import type {
   OptionsFiles,
   OptionsHasTypeScript,
   OptionsOverrides,
+  Rules,
   TypedFlatConfigItem,
 } from "../types";
 
@@ -54,25 +55,15 @@ export async function svelte(
           },
         ],
 
-        "svelte/comment-directive": "error",
-        "svelte/no-at-debug-tags": "warn",
-        "svelte/no-at-html-tags": "error",
-        "svelte/no-dupe-else-if-blocks": "error",
-        "svelte/no-dupe-style-properties": "error",
-        "svelte/no-dupe-use-directives": "error",
-        "svelte/no-export-load-in-svelte-module-in-kit-pages": "error",
-        "svelte/no-inner-declarations": "error",
-        "svelte/no-not-function-handler": "error",
-        "svelte/no-object-in-text-mustaches": "error",
-        "svelte/no-reactive-functions": "error",
-        "svelte/no-reactive-literals": "error",
-        "svelte/no-shorthand-style-property-overrides": "error",
-        "svelte/no-unknown-style-directive-property": "error",
-        "svelte/no-unused-svelte-ignore": "error",
-        "svelte/no-useless-mustaches": "error",
-        "svelte/require-store-callbacks-use-set-param": "error",
-        "svelte/system": "error",
-        "svelte/valid-each-key": "error",
+        ...pluginSvelte.configs.recommended
+          .map((config) => config.rules)
+          .reduce<Rules>(
+            (acc, rules) => ({
+              ...acc,
+              ...rules,
+            }),
+            {},
+          ),
 
         "unused-imports/no-unused-vars": [
           "error",
